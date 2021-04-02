4fe4de0baa7c1f0f57ec6f11327f05fc45e420d0
matevosyanmher <66181653+matevosyanmher@users.noreply.github.com>
2021-04-02 17:26:37 +0400

    Initial commit

---
+++ src/Chepter_03/Constants2.java
@@ -0,0 +1,18 @@
+package Chepter_03;
+
+public class Constants2 {
+    public static final double CM_PER_INCH = 2.54;
+
+    public static void main(String[] args) {
+
+        double paperWidth = 8.5;
+        double paperHeight = 11;
+
+        System.out.println("Paper size in centimeters: "
+                + paperWidth * CM_PER_INCH + " Ьу "
+                + paperHeight * CM_PER_INCH);
+        double x=0;
+         x +=3.5;
+        System.out.println(x);
+    }
+}
